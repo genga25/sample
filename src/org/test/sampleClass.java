4f509e5d4c302fb06c598207fb7580f091d9ebf1
genga25 <gengadevi25@gmail.com>
2022-06-23 15:26:21 +0530

    done

---
+++ src/org/test/sampleClass.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class sampleClass {
+	public static void main(String[] args) {
+		System.out.println("gengadevi");
+		System.out.println("github");
+		System.out.println("test");
+	}
+
+}
